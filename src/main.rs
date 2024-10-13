77c56722595d60c8adaeebe4f1634c9abf3bd230
Rafael de Paulo <rafaeldepaulo7@hotmail.com>
2024-10-13 10:38:59 -0300

    bugs corrigidos

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,11 @@ use rand::prelude::*;
 
 fn main() -> std::io::Result<()>{
     let mut rng = rand::thread_rng();
-    if !fs::exists("final-theme.css").is_err(){
-        fs::remove_file("final-theme.css")?;
-    }
     let mut entries = fs::read_dir("sorting")?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
     entries.shuffle(&mut rng);
-    let mut target = File::create_new("final-theme.css")?;
+    let mut target = File::create("final-theme.css")?;
     let mut source = File::open(entries.clone()[0].as_path())?;
     let mut content = String::new();
     source.read_to_string(&mut content)?;
